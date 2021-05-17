1837daf33b1bbdd306bc59da6afe88948c8d25c2
Mega Farts <clint.m.thompson@outlook.com>
2021-05-17 13:20:59 -0500

    Created function to allow computer to return a random selection from an array

--- choices.py
+++ choices.py
@@ -3,3 +3,6 @@ class Choices:
 
     def __init__(self):
         pass
+
+    def return_name(self):
+        return self.name
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from rock import Rock
 from paper import Paper
 from scissors import Scissors
@@ -11,6 +12,15 @@ if __name__ == '__main__':
     play_scissors = Scissors()
     play_lizard = Lizard()
     play_spock = Spock()
+
+
+def ai_turn():
+    ai_choices = [play_rock, play_paper, play_scissors, play_lizard, play_spock]
+    choice = ai_choices[random.randint(0, 4)].name
+    return choice
+
+
+print(ai_turn())
 
 
 
