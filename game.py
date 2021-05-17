7d72aef14b13113e0b4bb44488d90aa89b53f982
Mega Farts <clint.m.thompson@outlook.com>
2021-05-17 15:00:36 -0500

    Added the ability to play with 2 players

--- game.py
+++ game.py
@@ -50,6 +50,43 @@ def run_game():
             elif player_two_points == 3:
                 print('PLAYER TWO IS VICTORIOUS')
 
+    def vs_player():
+        player_one_points = 0
+        player_two_points = 0
+        while player_one_points < 3 and player_two_points < 3:
+            user_choice_one = input("Select from:\n0: Rock\n1: Paper\n2: Scissors\n3: Lizard\n4: Spock\nPlayer one input your choice")
+            user_result_one = list_of_choices[int(user_choice_one)]
+            user_choice_two = input("Player Two Input your choice\n________________________________")
+            user_result_two = list_of_choices[int(user_choice_two)]
+            print(f'Player One: {user_result_one.name}')
+            print(f'Player Two: {user_result_two.name}')
+            if user_result_one.beats[0] == user_result_two.name or user_result_one.beats[1] == user_result_two.name:
+                print('Player one wins the round')
+                player_one_points += 1
+                print(f'{player_one_points} - {player_two_points}')
+                print('________________________________')
+                print('________________________________')
+            elif user_result_one.loses_to[0] == user_result_two.name or user_result_one.loses_to[1] == user_result_two.name:
+                print('Player two wins the round')
+                player_two_points += 1
+                print(f'{player_one_points} - {player_two_points}')
+                print('________________________________')
+                print('________________________________')
+            elif user_result_one.name == user_result_two.name:
+                print("It's a tie, how boring")
+                print(f'{player_one_points} - {player_two_points}')
+                print('________________________________')
+                print('________________________________')
+            if player_one_points == 3:
+                print('PLAYER ONE IS VICTORIOUS')
+            elif player_two_points == 3:
+                print('PLAYER TWO IS VICTORIOUS')
+
+    player_select = input('1 or 2 players? (type 1 or 2)')
+    if int(player_select) == 1:
+        vs_computer()
+    elif int(player_select) == 2:
+        vs_player()
 
 
-    vs_computer()
+
